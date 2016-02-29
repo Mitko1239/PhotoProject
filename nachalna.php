5cd7117a7ac621eb8b11cb629b24116a8e61253a
Mitko1239 <dimitarcho1@gmail.com>
2016-02-29 18:44:39 +0200

    Profiles and fixes

--- nachalna.php
+++ nachalna.php
@@ -20,20 +20,20 @@
         <div id="registerChoose">
             <div class="form">
                 <a href="#">
-                    <p><img src="img/camera-clipart.jpg" alt=""></p>
+                    <p><img class="homePageImg" src="img/camera-clipart.jpg" alt=""></p>
                     <p>Регистрация като фотограф</p>
                 </a>
             </div>
             <div class="form">
                 <a href="#">
-                    <p><img src="img/model.png" alt=""></p>
+                    <p><img class="homePageImg" src="img/model.png" alt=""></p>
                     <p>Регистрация като модел</p>
                 </a>
             </div>
             <!--TODO: страница с най-високо оценени снимки-->
             <div class="form">
                 <a href="#">
-                    <p><img src="img/login.png" alt=""></p>
+                    <p><img class="homePageImg" src="img/login.png" alt=""></p>
                     <p>Продължи без регистрация</p>
                 </a>
             </div>
---
+++ profile.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dimit
+ * Date: 29.2.2016 г.
+ * Time: 17:44
+ */
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Профил</title>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="utf-8">
+</head>
+<body>
+<div id="container">
+    <nav id="top">
+        <ul>
+            <li><h3>Site Name</h3></li>
+            <li><input type="email" placeholder="И-мейл"></li>
+            <li><input type="password" placeholder="Парола"></li>
+            <li><input type="submit" value="Вход"></li>
+        </ul>
+    </nav>
+
+    <div class="siteBox">
+        <div id="profileBox">
+            <div id="cover">
+                <div id="row1">
+                    <div id="profilePic"><img src="https://placehold.it/150x150" alt=""></div>
+                    <div id="profileName"><h1>Username</h1></div>
+                </div>
+            </div>
+
+            <nav>
+                <ul>
+                    <li><a href="#">Информация</a></li>
+                    <li><a href="#">Снимки</a></li>
+                    <li><a href="#">Контакти</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+
+    <footer>
+        &copy; 2016 Whatever Corp.
+    </footer>
+
+</div>
+</body>
+</html>
