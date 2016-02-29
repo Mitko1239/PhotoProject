467d90118e63f086b5ff1ce69ac3107e6ddf9e14
Mitko1239 <dimitarcho1@gmail.com>
2016-02-29 17:43:11 +0200

    Menu & Boxes =)

--- nachalna.php
+++ nachalna.php
@@ -8,29 +8,36 @@
 </head>
 <body>
     <div id="container">
+        <nav>
+            <ul>
+                <li><h3>Site Name</h3></li>
+                <li><input type="email" placeholder="И-мейл"></li>
+                <li><input type="password" placeholder="Парола"></li>
+                <li><input type="submit" value="Вход"></li>
+            </ul>
+        </nav>
 
         <div id="registerChoose">
             <div class="form">
-                <p><img src="img/camera-clipart.jpg" alt=""></p>
-                <p>Регистрация като фотограф</p>
+                <a href="#">
+                    <p><img src="img/camera-clipart.jpg" alt=""></p>
+                    <p>Регистрация като фотограф</p>
+                </a>
             </div>
             <div class="form">
-                <p><img src="" alt=""></p>
-                <p>Регистрация като модел</p>
+                <a href="#">
+                    <p><img src="img/model.png" alt=""></p>
+                    <p>Регистрация като модел</p>
+                </a>
             </div>
+            <!--TODO: страница с най-високо оценени снимки-->
             <div class="form">
-                <p><img src="" alt=""></p>
-                <p>Вход</p>
+                <a href="#">
+                    <p><img src="img/login.png" alt=""></p>
+                    <p>Продължи без регистрация</p>
+                </a>
             </div>
         </div>
-
-<!--        <div class="form" id="registerChoose">-->
-<!--            <form action="">-->
-<!--                <input type="text"><br><br>-->
-<!--                <input type="text"><br><br>-->
-<!--                <input type="text">-->
-<!--            </form>-->
-<!--        </div>-->
     </div>
 </body>
 </html>
